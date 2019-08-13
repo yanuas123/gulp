dbbcde6f14853454450ba690f47e29d749c9472a
yanuas123 <januariyy@gmail.com>
2019-08-14 01:39:06 +0300

    fix routes

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,147 @@
 /* Routes ------------------------------------------------------------------- */
 const routes = {
 	images: {
-		src: "src/img/**/*.+(jpg|jpeg|JPG|JPEG|png|PNG|svg)",
-		build_src: "src/img/**/*.svg",
-		src_kraken: "src/img/**/*.+(jpg|jpeg|JPG|JPEG|png|PNG)",
-		dest: "dest/images/",
-		build: "build/images/",
-		watch: "src/img/**/*.+(jpg|jpeg|JPG|JPEG|png|PNG|svg)"
-	},
-	js: {
-		ts: {
-			src_ts: "src/ts/index.tslink.ts",
-			temporary: "temp/ts/",
-			temp_file_name: "index.ts",
-			dest_file_name: "index.js",
-			watch: "src/ts/**/*.ts",
-			types_src: "src/ts/@types/**/*.ts",
-			types_dest: "temp/ts/@types/"
+		all: {
+			src: {
+				all: "src/img/**/*.+(jpg|jpeg|JPG|JPEG|png|PNG|svg)",
+				build: "src/img/**/*.svg"
+			},
+			test: {
+				dir: "test/images/"
+			},
+			build: {
+				dir: "build/images/"
+			},
+			watch: {
+				all: "src/img/**/*.+(jpg|jpeg|JPG|JPEG|png|PNG|svg)"
+			}
 		},
-		js: {
-			temporary: "temp/js/",
-			src_js: "src/js/*.js",
-			dest: "dest/js/",
-			build: "build/js/"
+		kraken: {
+			src: {
+				build: "src/img/**/*.svg"
+			}
+		},
+		mockup: {
+			src: {
+				all: "src/img/mock-up/*.jpg"
+			},
+			test: {
+				dir: "test/mock-up/"
+			}
 		}
 	},
-	sass: {
-		src: "src/sass/style.scss",
-		dest: "dest/css/",
-		watch: "src/sass/**/*.scss"
+	js: {
+		all: {
+			src: {
+				all: ["src/js/*.js", "!src/js/test.js"],
+				test: "src/js/test.js",
+				build: ["src/js/*.js", "!src/js/test.js", "temp/js/index.js"]
+			},
+			temp: {
+				dir: "temp/js/"
+			},
+			test: {
+				dir: "test/js/"
+			},
+			build: {
+				dir: "build/js/"
+			},
+			watch: {
+				all: "src/ts/**/*.js",
+				test: "src/js/test.js"
+			}
+		},
+		ts: {
+			src: {
+				all: "src/ts/index.tslink.ts",
+				types: "src/ts/@types/**/*.ts"
+			},
+			temp: {
+				dir: "temp/ts/",
+				file: "index.ts",
+				types: "temp/ts/@types/"
+			},
+			watch: {
+				all: "src/ts/**/*.ts"
+			}
+		}
 	},
 	style: {
-		src: "src/sass/**/*.css",
-		build_src: ["dest/css/**/*.css", "!dest/css/test.css"],
-		dest: "dest/css/",
-		build: "build/css/",
-		watch: "src/sass/**/*.css"
+		all: {
+			src: {
+				test: "src/sass/**/*.css",
+				build: ["test/css/**/*.css", "!test/css/test.css"]
+			},
+			test: {
+				dir: "test/css/"
+			},
+			build: {
+				dir: "build/css/"
+			},
+			watch: {
+				all: "src/sass/**/*.css"
+			}
+		},
+		sass: {
+			src: {
+				all: "src/sass/style.scss"
+			},
+			watch: {
+				all: "src/sass/**/*.scss"
+			}
+		}
 	},
 	html: {
-		src: ["src/**/*.html"],
-		src_temp: "temp/html/*.html",
-		build_temp: "temp/html/",
-		dest: "dest/",
-		build: "build/",
-		watch: ["src/*.html", "src/partials/**/*.html"]
+		all: {
+			watch: {
+				all: ["src/*.html", "src/partials/**/*.html"]
+			}
+		},
+		replace_string: {
+			src: {
+				all: "src/**/*.html"
+			},
+			temp: {
+				dir: "temp/html/"
+			}
+		},
+		html_include: {
+			src: {
+				all: "temp/html/*.html"
+			},
+			test: {
+				dir: "test/"
+			},
+			build: {
+				dir: "build/"
+			}
+		}
 	},
 	plugins: {
-		src: "src/plugins/**/*",
-		dest: "dest/plugins/",
-		build: "build/plugins/"
+		all: {
+			src: {
+				all: "src/plugins/**/*"
+			},
+			test: {
+				dir: "test/plugins/"
+			},
+			build: {
+				dir: "build/plugins/"
+			}
+		}
 	},
 	fonts: {
-		src: "src/fonts/**/*.+(ttf|otf|woff|eot)",
-		dest: "dest/fonts/",
-		build: "build/fonts/"
+		all: {
+			src: {
+				all: "src/fonts/**/*.+(ttf|otf|woff|eot)"
+			},
+			test: {
+				dir: "test/fonts/"
+			},
+			build: {
+				dir: "build/fonts/"
+			}
+		}
 	},
 	server: {
 		path: "server.js"
@@ -236,7 +324,7 @@ function clean() {
 	return DEL(clean_dest + "**/*");
 }
 function cleanAll() {
-	let paths = [routes.images.build + "**/*", routes.js.js.build + "**/*", routes.style.build + "**/*", routes.html.build + "**/*.html", routes.plugins.build + "**/*", routes.fonts.build + "**/*"];
+	let paths = [routes.images.all.build.dir + "**/*", routes.style.all.build.dir + "**/*", routes.html.html_include.build.dir + "**/*.html", routes.plugins.all.build.dir + "**/*", routes.fonts.all.build.dir + "**/*", routes.js.all.build.dir + "**/*"];
 	return DEL(paths);
 }
 
@@ -256,20 +344,20 @@ function buildBrowser() {
 
 // typescript compilation
 function putTypes() {
-	return src(routes.js.ts.types_src)
-		.pipe(dest(routes.js.ts.types_dest));
+	return src(routes.js.ts.src.types)
+		.pipe(dest(routes.js.ts.temp.types));
 }
 function concatTs() {
-	return src(routes.js.ts.src_ts, {
+	return src(routes.js.ts.src.all, {
 		buffer: false
 	})
-		.pipe(TS_LINK(routes.js.ts.temp_file_name))
-		.pipe(dest(routes.js.ts.temporary));
+		.pipe(TS_LINK(routes.js.ts.temp.file))
+		.pipe(dest(routes.js.ts.temp.dir));
 }
 function compileTS() {
 	return TS_PROJECT.src()
 		.pipe(TS_PROJECT())
-		.js.pipe(dest(routes.js.js.temporary));
+		.js.pipe(dest(routes.js.all.temp.dir));
 }
 let constructTS = series(putTypes, concatTs, compileTS);
 
@@ -278,136 +366,136 @@ let constructTS = series(putTypes, concatTs, compileTS);
 
 /* TEST functions ----------------------------------------------------------- */
 function t_PutImages() {
-	return src(routes.images.src)
-		.pipe(dest(routes.images.dest));
+	return src(routes.images.all.src.all)
+		.pipe(dest(routes.images.all.test.dir));
 }
 
 function t_PutJs() {
-	return src(routes.js.js.src_js)
-		.pipe(dest(routes.js.js.dest));
+	return src(routes.js.all.src.test)
+		.pipe(dest(routes.js.all.test.dir));
 }
 
 function t_CompileSass() {
-	return src(routes.sass.src)
+	return src(routes.style.sass.src.all)
 		.pipe(SASS(prm.sass.all).on('error', SASS.logError))
-		.pipe(dest(routes.sass.dest));
+		.pipe(dest(routes.style.all.test.dir));
 }
 
 function t_PutCss() {
-	return src(routes.style.src)
+	return src(routes.style.all.src.test)
 		.pipe(CACHE())
-		.pipe(dest(routes.style.dest));
+		.pipe(dest(routes.style.all.test.dir));
 }
 
 function t_DelHtmlTestMark() {
 	let prop = prm.replace_string.del_loop.test;
-	return src(routes.html.src)
+	return src(routes.html.replace_string.src.all)
 		.pipe(REPLACE_STRING.replace(prop.plain.s, prop.plain.r))
-		.pipe(dest(routes.html.build_temp));
+		.pipe(dest(routes.html.replace_string.temp.dir));
 }
 function t_CompileHtml() {
 	prm.include.test.context.state = "test";
-	return src(routes.html.src_temp)
+	return src(routes.html.html_include.src.all)
 		.pipe(HTML_INCLUDE(prm.html_include.all))
-		.pipe(dest(routes.html.dest));
+		.pipe(dest(routes.html.html_include.test.dir));
 }
 let t_ConstructHtml = series(t_DelHtmlTestMark, t_CompileHtml);
 
 function t_PutPlugins() {
-	return src(routes.plugins.src)
-		.pipe(dest(routes.plugins.dest));
+	return src(routes.plugins.all.src.all)
+		.pipe(dest(routes.plugins.all.test.dir));
 }
 
 function t_PutFonts() {
-	return src(routes.fonts.src)
-		.pipe(dest(routes.fonts.dest));
+	return src(routes.fonts.all.src.all)
+		.pipe(dest(routes.fonts.all.test.dir));
 }
 
 function t_PutMockUpImages() {
-	return src(routes.fonts.src)
-		.pipe(dest(routes.fonts.dest));
+	return src(routes.images.mockup.src.all)
+		.pipe(dest(routes.images.mockup.test.dir));
 }
 
 /* BUILD functions ---------------------------------------------------------- */
 function b_PutImages() {
-	return src(routes.images.src)
-		.pipe(dest(routes.images.build));
+	return src(routes.images.all.src.all)
+		.pipe(dest(routes.images.all.build.dir));
 }
 
 function b_CompileTS() {
 	return TS_PROJECT.src()
 		.pipe(TS_PROJECT())
-		.js.pipe(dest(routes.js.js.build));
+		.js.pipe(dest(routes.js.all.build.dir));
 }
 let b_ConstructTS = series(putTypes, concatTs, b_CompileTS);
 function build1_put_js() {
-	return src([routes.js.js.src_js, "!src/js/test.js"])
-		.pipe(dest(routes.js.js.build));
+	return src(routes.js.all.src.all)
+		.pipe(dest(routes.js.all.build.dir));
 }
 
 function b_CompileSass() {
-	return src(routes.sass.src)
+	return src(routes.style.sass.src.all)
 		.pipe(SASS(prm.sass.all).on('error', SASS.logError))
-		.pipe(dest(routes.style.build));
+		.pipe(dest(routes.style.all.build.dir));
 }
 
 function b_PutCss() {
-	return src(routes.style.src)
+	return src(routes.style.all.src.build)
 		.pipe(CACHE())
-		.pipe(dest(routes.style.build));
+		.pipe(dest(routes.style.all.build.dir));
 }
 
 function b_DelHtmlLoopsAndTestStrings() {
 	let prop = prm.replace_string.del_loop.build.node;
-	return src(routes.html.src)
+	return src(routes.html.replace_string.src.all)
 		.pipe(REPLACE_STRING.replace(prop.plain.s, prop.plain.r))
 		.pipe(REPLACE_STRING.replace(prop.loop.s, prop.loop.r))
 		.pipe(REPLACE_STRING.replace(prop.insert1.s, prop.insert1.r))
-		.pipe(dest(routes.html.build_temp));
+		.pipe(dest(routes.html.replace_string.temp.dir));
 }
 function b_CompileHtml() {
 	prm.include.build.context.state = "none";
-	return src(routes.html.build_temp + "*.html")
+	return src(routes.html.html_include.src.all)
 		.pipe(HTML_INCLUDE(prm.html_include.all))
-		.pipe(dest(routes.html.build));
+		.pipe(dest(routes.html.html_include.build.dir));
 }
 let b_ConstructHtml = series(b_DelHtmlLoopsAndTestStrings, b_CompileHtml);
 
 function b_PutPlugins() {
-	return src(routes.plugins.src)
-		.pipe(dest(routes.plugins.build));
+	return src(routes.plugins.all.src.all)
+		.pipe(dest(routes.plugins.all.build.dir));
 }
 
 function b_PutFonts() {
-	return src(routes.fonts.src)
-		.pipe(dest(routes.fonts.build));
+	return src(routes.fonts.all.src.all)
+		.pipe(dest(routes.fonts.all.build.dir));
 }
 
 /* BUILD_END functions ------------------------------------------------------ */
 function e_PutSvg() {
-	return src(routes.images.build_src)
-		.pipe(dest(routes.images.build));
+	return src(routes.images.all.src.build)
+		.pipe(dest(routes.images.all.build.dir));
 }
 function e_OptimizeImages() {
-	return src(routes.images.src_kraken)
+	return src(routes.images.kraken.src.build)
 		.pipe(KRAKEN(prm.kraken.all))
-		.pipe(dest(routes.images.build));
+		.pipe(dest(routes.images.all.build.dir));
 }
 let e_ConstructImages = parallel(e_PutSvg, e_OptimizeImages);
 
 function e_OptimizeJS() {
-	return src([routes.js.js.temporary + routes.js.ts.dest_file_name, "!src/js/test.js", routes.js.js.src_js])
+	return src(routes.js.all.src.build)
 		.pipe(REMOVE_CONSOLE(prm.remove_console.all))
 		.pipe(UGLIFY(prm.uglify.all))
-		.pipe(dest(routes.js.js.build));
+		.pipe(dest(routes.js.all.build.dir));
 }
 let e_ConstructJS = series(constructTS, e_OptimizeJS);
 
 function e_OptimizeCss() {
-	return src(routes.style.build_src)
+	return src(routes.style.all.src.build)
 		.pipe(MEDIA_QUERIES(prm.media_queries.all))
 		.pipe(AUTOPREFIXER(prm.autoprefixer.all))
-		.pipe(dest(routes.style.build));
+		.pipe(dest(routes.style.all.build.dir));
 }
 let e_ConstructCss = series(t_CompileSass, t_PutCss, e_OptimizeCss);
 
@@ -415,19 +503,19 @@ function e_DelHtmlLoopsAndTestStrings() {
 	let prop;
 	if(prm.replace_string.del_loop.mode == "node") prop = prm.replace_string.del_loop.build.node;
 	else if(prm.replace_string.del_loop.mode == "php") prop = prm.replace_string.del_loop.build.php;
-	return src(routes.html.src)
+	return src(routes.html.replace_string.src.all)
 		.pipe(REPLACE_STRING.replace(prop.plain.s, prop.plain.r))
 		.pipe(REPLACE_STRING.replace(prop.attr.s, prop.attr.r))
 		.pipe(REPLACE_STRING.replace(prop.loop.s, prop.loop.r))
 		.pipe(REPLACE_STRING.replace(prop.insert1.s, prop.insert1.r))
-		.pipe(dest(routes.html.build_temp));
+		.pipe(dest(routes.html.replace_string.temp.dir));
 }
 function e_CompileHtml() {
 	prm.include.build.context.state = "none";
-	return src(routes.html.build_temp + "*.html")
+	return src(routes.html.html_include.src.all)
 		.pipe(HTML_INCLUDE(prm.html_include.all))
 		.pipe(HTML_BEAUTIFY(prm.html_beautify.all))
-		.pipe(dest(routes.html.build));
+		.pipe(dest(routes.html.html_include.build.dir));
 }
 let e_ConstructHtml = series(e_DelHtmlLoopsAndTestStrings, e_CompileHtml);
 
@@ -438,23 +526,23 @@ let e_ConstructHtml = series(e_DelHtmlLoopsAndTestStrings, e_CompileHtml);
 exports.default = function() {
 	testBrowser();
 	parallel(t_PutPlugins, t_PutFonts, t_PutMockUpImages)();
-	watch(routes.images.watch, prm.watch.all.param, series(t_PutImages, testReload));
-	watch(routes.js.js.src_js, prm.watch.all.param, series(t_PutJs, testReload));
-	watch(routes.style.watch, prm.watch.all.param, series(t_PutCss, testReload));
-	watch(routes.sass.watch, prm.watch.all.param, series(t_CompileSass, testReload));
-	watch(routes.html.watch, prm.watch.all.param, series(t_ConstructHtml, testReload));
+	watch(routes.images.all.watch.all, prm.watch.all.param, series(t_PutImages, testReload));
+	watch(routes.js.all.watch.test, prm.watch.all.param, series(t_PutJs, testReload));
+	watch(routes.style.all.watch.all, prm.watch.all.param, series(t_PutCss, testReload));
+	watch(routes.style.sass.watch.all, prm.watch.all.param, series(t_CompileSass, testReload));
+	watch(routes.html.all.watch.all, prm.watch.all.param, series(t_ConstructHtml, testReload));
 };
 
 // build - compile dirty files to build; command - gulp build
 exports.build = function() {
 	buildBrowser();
 	parallel(b_PutPlugins, b_PutFonts)();
-	watch(routes.images.watch, prm.watch.all.param, series(b_PutImages));
-	watch(routes.js.ts.watch, prm.watch.all.param, series(b_ConstructTS));
-	watch(routes.js.js.src_js, prm.watch.all.param, series(build1_put_js));
-	watch(routes.style.watch, prm.watch.all.param, series(b_PutCss));
-	watch(routes.sass.watch, prm.watch.all.param, series(b_CompileSass));
-	watch(routes.html.watch, prm.watch.all.param, series(b_ConstructHtml));
+	watch(routes.images.all.watch.all, prm.watch.all.param, series(b_PutImages));
+	watch(routes.js.ts.watch.all, prm.watch.all.param, series(b_ConstructTS));
+	watch(routes.js.all.watch.all, prm.watch.all.param, series(build1_put_js));
+	watch(routes.style.all.watch.all, prm.watch.all.param, series(b_PutCss));
+	watch(routes.style.sass.watch.all, prm.watch.all.param, series(b_CompileSass));
+	watch(routes.html.all.watch.all, prm.watch.all.param, series(b_ConstructHtml));
 };
 
 // build_end - compile optimized files to build; command - gulp build_end
